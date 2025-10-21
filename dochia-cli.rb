6f1f98a232c1f79d756e1d5fc06824138ed2769f
ludovicianul <iliemad@gmail.com>
2025-10-21 21:21:46 +0300

    Update to latest version of dochia-cli

--- dochia-cli.rb
+++ dochia-cli.rb
@@ -7,22 +7,22 @@ class DochiaCli < Formula
 
   if OS.mac?
       if Hardware::CPU.intel?
-        url "https://github.com/dochia-dev/dochia-cli/releases/download/dochia-cli-1.1.0/dochia-cli_macos_amd64_1.1.0.tar.gz"
-        sha256 "70aa9b8ff1805616b15bf05251751419c4a49c866c21cd11148655078077aa6b"
+        url "https://github.com/dochia-dev/dochia-cli/releases/download/dochia-cli-1.2.0/dochia-cli_macos_amd64_1.2.0.tar.gz"
+        sha256 "70f5dc634c98544bcfe589ec53fc3b6f6fa311ddeb0095a84dfa27b557332ffb"
       elsif Hardware::CPU.arm?
-        url "https://github.com/dochia-dev/dochia-cli/releases/download/dochia-cli-1.1.0/dochia-cli_macos_arm64_1.1.0.tar.gz"
-        sha256 "93979cf06f71870266239d449a522ba1252fe969653b8a090620881ac6e054ed"
+        url "https://github.com/dochia-dev/dochia-cli/releases/download/dochia-cli-1.2.0/dochia-cli_macos_arm64_1.2.0.tar.gz"
+        sha256 "5c817fc48b27851a9f5df94102fabf23569d5557f8325c8c886b8ac461474ffe"
       end
   elsif OS.linux?
       if Hardware::CPU.intel?
-        url "https://github.com/dochia-dev/dochia-cli/releases/download/dochia-cli-1.1.0/dochia-cli_linux_amd64_1.1.0.tar.gz"
-        sha256 "8ad53811d877905a96c0f4dd2d51d13eed6424530fbfb9fab049172dae634afd"
+        url "https://github.com/dochia-dev/dochia-cli/releases/download/dochia-cli-1.2.0/dochia-cli_linux_amd64_1.2.0.tar.gz"
+        sha256 "9d2562770243f20a5a6e7bd6beb299eaa6eb3700a611fa0adb09dc4e5dce8d0c"
       elsif Hardware::CPU.arm?
-        url "https://github.com/dochia-dev/dochia-cli/releases/download/dochia-cli-1.1.0/dochia-cli_linux_arm64_1.1.0.tar.gz"
-        sha256 "9ae84274e94f97186f355ad272e3ce84c057438ae9a6d915c359db91f8cf8faa"
+        url "https://github.com/dochia-dev/dochia-cli/releases/download/dochia-cli-1.2.0/dochia-cli_linux_arm64_1.2.0.tar.gz"
+        sha256 "240a355b73c33e51c93d9b518561eeaa4bcb05247112392a2e2c5b7e44eb823b"
       end
   else
-    odie "Please use the uberjar version: https://github.com/dochia-dev/dochia-cli/releases/download/dochia-cli-1.1.0/dochia-cli_uberjar_1.1.0.tar.gz"
+    odie "Please use the uberjar version: https://github.com/dochia-dev/dochia-cli/releases/download/dochia-cli-1.2.0/dochia-cli_uberjar_1.2.0.tar.gz"
   end
 
   license "Apache-2.0"
